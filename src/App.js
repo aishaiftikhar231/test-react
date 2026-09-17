87353930865f54d7abd0b19f05c649b7b2f3bb51
aishaiftikhar231 <aishathewitch231@gmail.com>
2026-09-17 13:24:58 +0500

    hi

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          THis is a very bad app
         </a>
       </header>
     </div>
